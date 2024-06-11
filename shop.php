36b5681d53a3614473d396ffb50405c4a9c1a12e
antrc2 <antrc2gamer@gmail.com>
2024-06-11 13:36:11 +0700

    Add SweetAlert

--- shop.php
+++ shop.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap">
     <link rel="stylesheet" href="asset/CSS/nav.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
 
