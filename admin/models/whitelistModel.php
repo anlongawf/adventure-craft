98d6a5812b57a6a155a81a61e2927d2dda3da97a
antrc2 <antrc2gamer@gmail.com>
2024-06-11 22:09:04 +0700

    Update Whitelist Log

--- admin/models/whitelistModel.php
+++ admin/models/whitelistModel.php
@@ -12,6 +12,7 @@
         }
         function addWhitelist($name){
             sendDiscordMessage("https://discord.com/api/webhooks/1250064542893871104/Q0NMnTPCUGgPmO9kNBhMaHgIVUGVYWnREJIaCUb7lcsNpGdU9xOHVEs50OqNYYL-ZrVg","whitelist add $name");
+            sendDiscordMessage("https://discord.com/api/webhooks/1250103719689977897/GPMAW2kC9Wn0rHUS5IWDsF_AU09X27xW3tvZPoEObjS55qwvTGDsjC38dROjYV1By8QC",$_SESSION['taiKhoan']." đã add whitelist cho $name");
             $this->conn->prepare("UPDATE users SET whitelist=1 WHERE name='$name'")->execute();
             header("Location: ?act=white-list");
         }
